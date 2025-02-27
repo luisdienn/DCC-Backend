2fae1bafe7c64ce069a22d6149944b93a9e4673e
Alexander López Sánchez <Alopezs@ucenfotec.ac.cr>
2025-02-27 14:37:22 -0600

    prefix auth

--- Backend DCC/app/api/v1/endpoints/auth.py
+++ Backend DCC/app/api/v1/endpoints/auth.py
@@ -3,7 +3,7 @@ from app.services.auth_service import AuthService
 
 router = APIRouter()
 
-@router.get("/auth/google")
+@router.get("/google")
 async def login_google(access_token: str):
     user = await AuthService.authenticate_with_google(access_token)
     if not user:
