7530449f5d1d20ba681c513fdc272ed3337e90aa
luisdienn <ldienb@ucenfotec.ac.cr>
2025-03-09 15:52:59 -0600

    Actualizar Endpoints

--- Backend DCC/app/api/endpoints/professor.py
+++ Backend DCC/app/api/endpoints/professor.py
@@ -24,20 +24,20 @@ async def create_professor(professor: ProfessorResponseSchema, service: Professo
     return await service.create_professor(professor)
               
 #Read
-@router.get("/{professor_id}", response_model=ProfessorResponseSchema)
+@router.get("/getById/{professor_id}", response_model=ProfessorResponseSchema)
 async def get_professor(professor_id: str, service: ProfessorService = Depends(lambda: professor_service)):
     return await service.get_professor_by_id(professor_id)
 
-@router.get("/", response_model=list[ProfessorResponseSchema])
+@router.get("/getAll", response_model=list[ProfessorResponseSchema])
 async def get_all_professors(service: ProfessorService = Depends(lambda: professor_service)):
     return await service.get_professors()
 
 #Update
-@router.put("/{professor_id}", response_model=str)
+@router.put("/put/{professor_id}", response_model=str)
 async def update_professor(professor_id: str, professor: ProfessorResponseSchema, service: ProfessorService = Depends(lambda: professor_service)):
     return await service.update_professor(professor_id, professor)
 
 #Delete
-@router.delete("/{professor_id}", response_model=str)
+@router.delete("/delete/{professor_id}", response_model=str)
 async def delete_professor(professor_id: str, service: ProfessorService = Depends(lambda: professor_service)):
     return await service.delete_professor(professor_id)
