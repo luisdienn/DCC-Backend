3cb32aaa6837a3c836d38db0750a3bccc5010ed1
EmersonDHH <ehidalgoh@ucenfotec.ac.cr>
2025-04-21 14:21:16 -0600

    Update main.py

--- Backend DCC/app/main.py
+++ Backend DCC/app/main.py
@@ -49,7 +49,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",                     # Desarrollo local
-        "https://dcc-frontend-eight.vercel.app"      # Producción en Vercel
+        "https://dcc-frontend-git-main-luisdiegos-projects.vercel.app"      # Producción en Vercel
     ],
     allow_credentials=True,
     allow_methods=["*"],
